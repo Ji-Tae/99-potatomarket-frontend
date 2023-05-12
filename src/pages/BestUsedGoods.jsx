cc6dc93a611e17bbaf0d1238ff316fdeb042ee04
Ji-Tae <117277785+Ji-Tae@users.noreply.github.com>
2023-05-12 22:27:43 +0900

    [DOCS] 초기파일 3차 수정

---
+++ src/pages/BestUsedGoods.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function BestUsedGoods() {
+  return <div>BestUsedGoods</div>;
+}
+
+export default BestUsedGoods;
---
+++ src/pages/UsedGoods.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function UsedGoods() {
+  return <div>UsedGoods</div>;
+}
+
+export default UsedGoods;
