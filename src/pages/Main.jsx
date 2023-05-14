a4a100eb686d0d67e1913f0a0b0f09a052d04737
minnjeong <bbono4500@gmail.com>
2023-05-14 23:30:41 +0900

    [Feat] card

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React from 'react'
 
 function Main() {
-  return <div>main</div>;
+  return (
+    <div>Main</div>
+  )
 }
 
-export default Main;
+export default Main
--- src/pages/UserMyPage.jsx
+++ src/pages/UserMyPage.jsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import Card from '../components/common/Card';
+import { styled } from 'styled-components';
 
-function UserMyPage() {
-  return <div>UserMyPage</div>;
+function UsedGoods() {
+  return (
+<>
+    <Container>
+      {/* 관심목록 */}
+      <LikeList>
+        <LikeListTitle>
+        </LikeListTitle>
+        <Cards>
+          <Card width={`15%`} />
+        </Cards>
+      </LikeList>
+      {/* 판매중 */}
+      <SellingList>
+        <SellingListTitle>
+        </SellingListTitle>
+        <Cards>
+          <Card width={`15%`} />
+        </Cards>
+      </SellingList>
+      {/* 거래완료 */}
+      <DoneList>
+        <DoneListTitle>
+        </DoneListTitle>
+        <Cards>
+          <Card width={`15%`} />
+        </Cards>
+      </DoneList>
+    </Container>
+    </>
+  )
 }
 
-export default UserMyPage;
+export default UsedGoods;
+
+const Container = styled.div``
+const LikeList = styled.div``
+const LikeListTitle = styled.div``
+const Cards = styled.div``
+const SellingList = styled.div``
+const SellingListTitle = styled.div``
+const DoneList = styled.div``
+const DoneListTitle = styled.div``
