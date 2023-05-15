bff77d7ad982fbec6504f3bc6299f2c9100e3826
minnjeong <bbono4500@gmail.com>
2023-05-15 10:19:15 +0900

    [Docs]Cards

---
+++ src/components/common/Layout.js
@@ -0,0 +1,8 @@
+import  styled  from "styled-components"
+
+export const Layout = styled.div`
+  min-width: 800px;
+  max-width: 1300px;
+  margin: auto;
+  
+`
--- src/pages/UserMyPage.jsx
+++ src/pages/UserMyPage.jsx
@@ -1,46 +1,61 @@
 import React from 'react';
 import Card from '../components/common/Card';
-import { styled } from 'styled-components';
+import  styled  from 'styled-components';
+import { Layout } from '../components/common/Layout';
 
 function UsedGoods() {
   return (
-<>
-    <Container>
+<Layout>
       {/* 관심목록 */}
+      
       <LikeList>
         <LikeListTitle>
         </LikeListTitle>
         <Cards>
           <Card width={`15%`} />
+          <Card width={`15%`} />
+          <Card width={`15%`} />
         </Cards>
       </LikeList>
+      
       {/* 판매중 */}
+     
       <SellingList>
         <SellingListTitle>
         </SellingListTitle>
         <Cards>
           <Card width={`15%`} />
+          <Card width={`15%`} />
+          <Card width={`15%`} />
         </Cards>
       </SellingList>
+    
       {/* 거래완료 */}
+    
       <DoneList>
         <DoneListTitle>
         </DoneListTitle>
         <Cards>
           <Card width={`15%`} />
+          <Card width={`15%`} />
+          <Card width={`15%`} />
         </Cards>
       </DoneList>
-    </Container>
-    </>
+      
+    </Layout>
   )
 }
 
 export default UsedGoods;
 
-const Container = styled.div``
-const LikeList = styled.div``
+const LikeList = styled.div`
+width: 70%;
+`
 const LikeListTitle = styled.div``
-const Cards = styled.div``
+const Cards = styled.div`
+width:100%;
+display: flex;
+`
 const SellingList = styled.div``
 const SellingListTitle = styled.div``
 const DoneList = styled.div``
