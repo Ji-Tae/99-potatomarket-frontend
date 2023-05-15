e90312e0b394f5b4c536851c292594017083e0e9
Ji-Tae <117277785+Ji-Tae@users.noreply.github.com>
2023-05-15 10:49:14 +0900

    [Docs]카드 배열 수정

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -4,7 +4,7 @@ import { HeartOutlined, HeartFilled, MessageOutlined } from '@ant-design/icons';
 
 function Card({ width }) {
   return (
-    <CardArea width={width} margin={'0px 44px 56px 0px'} padding={'20px'}>
+    <CardArea width={width} padding={'20px'}>
       <CardPhoto>
         <img
           alt='갤럭시 Z 폴드 3 5G'
@@ -17,10 +17,12 @@ function Card({ width }) {
         <CardPrice>400,000원</CardPrice>
         <CardCounts>
           <span>
-          <HeartOutlined />&nbsp; 21
+            <HeartOutlined />
+            &nbsp; 21
           </span>
           <span>
-          <MessageOutlined />&nbsp; 5
+            <MessageOutlined />
+            &nbsp; 5
           </span>
         </CardCounts>
       </CardDesc>
@@ -64,41 +66,40 @@ const CardDesc = styled.div`
   margin-top: 12px;
 `;
 const CardTitle = styled.div`
-    font-size: 16px;
-    letter-spacing: -0.02px;
-    color: #212529;
-    overflow: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    margin-bottom: 4px;
-    line-height: 1.5;
-    font-weight: 800;
-    `
+  font-size: 16px;
+  letter-spacing: -0.02px;
+  color: #212529;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  margin-bottom: 4px;
+  line-height: 1.5;
+  font-weight: 800;
+`;
 
 const CardPrice = styled.div`
-color: #826464;
-font-size: 15px;
-    font-weight: 700;
-    line-height: 1.5;
-    margin-bottom: 4px;
-    `
+  color: #826464;
+  font-size: 15px;
+  font-weight: 700;
+  line-height: 1.5;
+  margin-bottom: 4px;
+`;
 const Location = styled.div`
-font-size: 13px;
-    color: #212529;
-    overflow: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    margin-bottom: 4px;
-    line-height: 1.5;
-`
-
+  font-size: 13px;
+  color: #212529;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  margin-bottom: 4px;
+  line-height: 1.5;
+`;
 
 const CardCounts = styled.div`
-padding-top: 8px;
-    font-size: 13px;
-    display: flex;
-    margin-left: 50%;
-    align-items: center;
-    justify-content: space-between;
-    font-size: 13px;
-    `
+  padding-top: 8px;
+  font-size: 13px;
+  display: flex;
+  margin-left: 50%;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 13px;
+`;
