07659cda56c376d40c3f7573460e7bf88a88e9fd
minnjeong <bbono4500@gmail.com>
2023-05-17 09:28:26 +0900

    [Feat] Upload 서버

--- src/api/posts.js
+++ src/api/posts.js
@@ -13,5 +13,15 @@ const uploadPost = async(formData) => {
   return response
 } 
 
-//
+// 게시글 상세 조회
+// const getGoodsDetail = async () => {
+//   const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api/posts/:post_id`, { withCredentials: true });
+//   return response.data;
+// };
+
+// const getGoodsDetail = async () => {
+//   const response = await axios.get("http://13.209.35.164:3000//api/posts/2");
+//   return response;
+// };
+
 export{uploadPost};
